6ea21a32b09ca0a5dc1df07033e1212645cafeb2
James Rock <hello@jamesrock.me>
2018-05-09 15:03:19 +0100

    updates

--- js/doubleornothing.js
+++ js/doubleornothing.js
@@ -9,56 +9,56 @@
 	outputInc = 0,
 	multiplier = 2,
 	deposited = 0,
-	winningsVal = 0,
-	walletVal = 0,
+	winnings = 0,
+	wallet = 0,
 	unencode = function(string) {
 
 		return ROCK.JQUERY.createNode("div").html(string).text();
 
 	},
 	toCurrency = function(val) {
-		
+
 		return ("&pound;" + val.toFixed(2));
 
 	},
 	depositAmount = function(amount) {
 
-		walletVal += amount;
+		wallet += amount;
 		deposited += amount;
-		button.html(toCurrency(walletVal)).attr("data-win", "");
+		button.html(toCurrency(wallet)).attr("data-win", "");
 		_gaq.push(["_trackEvent", user, "deposit", getOutput()]);
 
 	},
 	pressButton = function() {
-		
+
 		return (ROCK.MATH.random(1, multiplier)===1);
 
 	},
 	getOutput = function() {
 
-		var _return = (outputInc + ":" + deposited + "/" + winningsVal);
+		var _return = (outputInc + ":" + deposited + "/" + winnings);
 		outputInc ++;
 		return _return;
-	
+
 	},
 	app = ROCK.JQUERY.createNode("div").prependTo("body"),
 	buttonWrap = ROCK.JQUERY.createNode("div").appendTo(app),
 	button = ROCK.JQUERY.createNode("a").html(toCurrency(deposited)).attr("href", "#").attr("data-role", "button").on("click", function() {
-		
+
 		return false;
 
 	}).on("mousedown", function() {
-		
+
 		button.attr("data-win", "");
 
 	}).on("mouseup", function() {
 
-		var 
+		var
 		result = pressButton(),
 		toSet;
 
-		if(walletVal<=0) {
-			
+		if(wallet<=0) {
+
 			alert("You have 0 stake.");
 			button.attr("data-win", "");
 			return;
@@ -67,42 +67,42 @@
 
 		if(result) {
 
-			walletVal += (walletVal*multiplier);
+			wallet += (wallet*multiplier);
 
 		}
 		else {
 
-			walletVal -= walletVal;
+			wallet -= wallet;
 
 		};
 
-		winningsVal = (walletVal-deposited);
+		winnings = (wallet-deposited);
 
-		walletVal = ROCK.MATH.roundTo(walletVal, 100);
-		winningsVal = ROCK.MATH.roundTo(winningsVal, 100);
+		wallet = ROCK.MATH.roundTo(wallet, 100);
+		winnings = ROCK.MATH.roundTo(winnings, 100);
 
-		button.html(toCurrency(walletVal)).attr("data-win", result);
+		button.html(toCurrency(wallet)).attr("data-win", result);
 
 		_gaq.push(["_trackEvent", user, "button", getOutput()]);
-		
+
 		return false;
 
 	}).appendTo(buttonWrap),
 	depositsWrap = ROCK.JQUERY.createNode("div").appendTo(app),
 	deposit1 = ROCK.JQUERY.createNode("a").attr("data-role", "deposit").attr("title", "Deposit " + toCurrency(1)).attr("href", "#").html("+" + toCurrency(1)).appendTo(depositsWrap).on("click", function() {
-			
+
 			depositAmount(1);
 			return false;
 
 	}),
 	deposit5 = ROCK.JQUERY.createNode("a").attr("data-role", "deposit").attr("title", "Deposit " + toCurrency(5)).attr("href", "#").html("+" + toCurrency(5)).appendTo(depositsWrap).on("click", function() {
-			
+
 			depositAmount(5);
 			return false;
 
 	}),
 	deposit10 = ROCK.JQUERY.createNode("a").attr("data-role", "deposit").attr("title", "Deposit " + toCurrency(10)).attr("href", "#").html("+" + toCurrency(10)).appendTo(depositsWrap).on("click", function() {
-			
+
 			depositAmount(10);
 			return false;
 
@@ -110,10 +110,10 @@
 	finishWrap = ROCK.JQUERY.createNode("div").appendTo(app),
 	finish = ROCK.JQUERY.createNode("a").attr("href", "#").html("withdraw winnings").attr("title", "withdraw winnings").on("click", function() {
 
-		//console.log((user + ": " + deposited + "/" + winningsVal));
+		//console.log((user + ": " + deposited + "/" + winnings));
 		//_gaq.push(["_trackEvent", user, "finish", getOutput()]);
 
-		var message = unencode("Total Winnings:\n " + toCurrency(winningsVal));
+		var message = unencode("Total Winnings:\n " + toCurrency(winnings));
 
 		alert(message);
 
